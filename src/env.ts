5b558caeb020df1f156d632b0e7e955c51aeaef3
ET <pip2d06b@gmail.com>
2024-08-02 10:25:34 +0800

    chore: update env.ts for server and client configurations

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,22 @@
 import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
 
 export const Env = createEnv({
-  server: {},
+  server: {
+    DATABASE_URL: z.string().optional(),
+  },
 
-  client: {},
+  client: {
+    NEXT_PUBLIC_APP_URL: z.string().optional(),
+  },
 
-  experimental__runtimeEnv: {},
+  shared: {
+    NODE_ENV: z.enum(['test', 'development', 'production']),
+  },
+
+  runtimeEnv: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NODE_ENV: process.env.NODE_ENV,
+  },
 });
